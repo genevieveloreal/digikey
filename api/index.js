ff8121955bc7f56abb522c3b10a6ccea55682693
charltona <2724511+charltona@users.noreply.github.com>
2021-08-21 10:10:42 +1000

    Test API

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const express = require("express");
-const newsletter = require("./newsletter.js");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/newsletter", newsletter);
+app.get("/", (req, res) => {
+  res.send('hello chicken')
+});
 
 app.listen(8080, function () {
   console.log("Server listening on port 8080");
